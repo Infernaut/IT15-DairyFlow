d3625fd50609d98eeffaf77067ac1ca5ee1114c5
Infernaut <roiwilliam59@gmail.com>
2026-02-10 23:47:23 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT15_DairyFlow.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Views/Shared/_SidebarPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IT15_DairyFlow.Views.Shared
+{
+    public class _SidebarPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
